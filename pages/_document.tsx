adc2f459338bb6c079350146dd1cb8614289c143
Nandira Paturohman <nandira.paturohman@payfazz.com>
2022-04-07 15:05:40 +0700

    google verif

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="2FQs8V7ilW56zeGFAkvWRAs9wON8ZSsnCWszNZu4HNI" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
