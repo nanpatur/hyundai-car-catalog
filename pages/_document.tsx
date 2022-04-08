5ffde002ef4817f40008e6d6358d01d32e7d9494
Nandira Paturohman <nandira.paturohman@payfazz.com>
2022-04-08 09:36:02 +0700

    analitycs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,34 @@
-
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="google-site-verification" content="2FQs8V7ilW56zeGFAkvWRAs9wON8ZSsnCWszNZu4HNI" />
+        <meta
+          name="google-site-verification"
+          content="2FQs8V7ilW56zeGFAkvWRAs9wON8ZSsnCWszNZu4HNI"
+        />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=UA-225311222-1`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'UA-225311222-1');
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
