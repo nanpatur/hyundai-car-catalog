4c4d1a928b7be4ca0ffa5fd67cd1f406f1cce47b
Nandira Paturohman <nandira.paturohman@payfazz.com>
2022-04-25 12:07:32 +0700

    gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -49,8 +50,28 @@ export default function Document() {
           src="https://www.facebook.com/tr?id=7245061332234085&ev=PageView&noscript=1"
         /></noscript>
         {/* End Meta Pixel Code */}
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5M5LT9H');
+            `
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5M5LT9H"
+            height="0"
+            width="0"
+            style={{ display:'none', visibility: 'hidden' }}></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
